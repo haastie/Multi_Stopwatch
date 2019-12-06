04da6017765481f1408bb8d864c73be9fa24127e
CasSunderman <cassunderman@gmail.com>
2019-12-06 11:47:22 +0100

    Tweede scherm proberen te maken, nog geen succes

--- app/src/main/java/com/example/android/multistopwatch/MainActivity.kt
+++ app/src/main/java/com/example/android/multistopwatch/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.android.multistopwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Check if this works
+
+      quickstart.setOnClickListener {
+         d ( "Test", "quickstart pressed")
+          startActivity(Intent(this, quickstart::class.java ))
+      }
     }
 }
